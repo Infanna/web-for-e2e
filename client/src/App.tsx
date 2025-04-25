b349443ca0c79222cb3d72596b9cd1224477dd4c
Pitchamint <>
2025-04-25 17:13:33 +0700

    update: folderand layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-
-import './App.scss';
-import { RouterProvider } from 'react-router-dom';
-import AppRouters from './router/AppRouters';
+import "./App.scss";
+import { RouterProvider } from "react-router-dom";
+import AppRouters from "./router/AppRouters";
 
 export default function MyApp() {
   console.log("Mock server started");
   return <RouterProvider router={AppRouters} />;
 }
-
---
+++ client/src/layout/TamplateLayout.tsx
@@ -0,0 +1,1 @@
+export function TemplateLayout({ children }: { children: React.ReactNode }) {}
