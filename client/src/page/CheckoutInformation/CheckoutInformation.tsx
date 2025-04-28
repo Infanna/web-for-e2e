9bf5071909b80bd1de6dbd4bce14b3ceda0da999
miles <miles@odds.team>
2025-04-28 12:59:48 +0700

    style: update checkout information layout

--- client/src/page/CheckoutInformation/CheckoutInformation.tsx
+++ client/src/page/CheckoutInformation/CheckoutInformation.tsx
@@ -9,123 +9,125 @@ const CheckoutInformation: React.FC = () => {
   return (
     <div className="container">
       <form
-        className="needs-validation d-flex flex-column row-gap-3"
+        className="needs-validation d-grid pt-3"
+        style={{ gridTemplateRows: "auto 1fr auto", height: "90vh" }}
         noValidate
         onSubmit={formSubmit}
         action={action}
       >
         <div className="row">
-          <h2> Checkout </h2>
+          <h4> Checkout information </h4>
         </div>
-        <div className="row">
-          <div className="col d-flex flex-column row-gap-2">
-            <div className="row">
-              <div className="col">
-                <select
-                  className="form-select"
-                  aria-label="Default select example"
-                  name="title"
-                  required
-                >
-                  <option value="">Select</option>
-                  <option value="Mr.">Mr.</option>
-                  <option value="Mrs.">Mrs.</option>
-                  <option value="None">None</option>
-                </select>
+        <div className="row justify-content-center py-5">
+          <div className="w-75">
+            <div className="col d-flex flex-column row-gap-2">
+              <div className="row">
+                <div className="col">Name</div>
               </div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="house-number"
-                  placeholder="House number"
-                  aria-label="House number"
-                  required
-                />
+              <div className="row">
+                <div className="col-auto">
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    name="title"
+                    required
+                  >
+                    <option value="">Select title *</option>
+                    <option value="Mr.">Mr.</option>
+                    <option value="Mrs.">Mrs.</option>
+                    <option value="None">None</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="first-name"
-                  placeholder="Firstname"
-                  aria-label="Firstname"
-                  required
-                />
+              <div className="row">
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="first-name"
+                    placeholder="Firstname *"
+                    aria-label="Firstname"
+                    required
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="lastname"
+                    placeholder="Lastname *"
+                    aria-label="Firstname"
+                    required
+                  />
+                </div>
               </div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="street"
-                  placeholder="Street"
-                  aria-label="Street"
-                />
+              <div className="row">
+                <div className="col">Address</div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="lastname"
-                  placeholder="Lastname"
-                  aria-label="Firstname"
-                  required
-                />
+              <div className="row">
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="house-number"
+                    placeholder="House number *"
+                    aria-label="House number"
+                    required
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="street"
+                    placeholder="Street"
+                    aria-label="Street"
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="sub-district"
+                    placeholder="Sub disctrict *"
+                    aria-label="Sub disctrict"
+                    required
+                  />
+                </div>
               </div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="sub-district"
-                  placeholder="Sub disctrict"
-                  aria-label="Sub disctrict"
-                  required
-                />
-              </div>
-            </div>
+              <div className="row">
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="district"
+                    placeholder="District *"
+                    aria-label="District"
+                    required
+                  />
+                </div>
 
-            <div className="row">
-              <div className="col"></div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="district"
-                  placeholder="District"
-                  aria-label="District"
-                  required
-                />
-              </div>
-            </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="province"
+                    placeholder="Province *"
+                    aria-label="Province"
+                    required
+                  />
+                </div>
 
-            <div className="row">
-              <div className="col"></div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="province"
-                  placeholder="Province"
-                  aria-label="Province"
-                  required
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col"></div>
-              <div className="col">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="postal-code"
-                  placeholder="Postalcode"
-                  aria-label="Postalcode"
-                  required
-                />
+                <div className="col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="postal-code"
+                    placeholder="Postalcode *"
+                    aria-label="Postalcode"
+                    required
+                  />
+                </div>
               </div>
             </div>
           </div>
