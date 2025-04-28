f3c69654a705d444c01a9cbe2fc03a1b4173c2b7
miles <miles@odds.team>
2025-04-28 16:10:12 +0700

    chore: add customer information interface

---
+++ client/src/shared/interface/customer.interface.ts
@@ -0,0 +1,11 @@
+export interface CustomerInformation {
+  title: string;
+  firstname: string;
+  lastname: string;
+  houseNumber: string;
+  street?: string;
+  subDistrict: string;
+  district: string;
+  province: string;
+  postalCode: string;
+}
