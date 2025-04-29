97c4f28414477e91aac27bf240a7fbdc6ba1fbbd
Infanna <patcharachart@odds.team>
2025-04-29 14:11:08 +0700

    update: reactor router guard

--- client/src/router/AppRouters.tsx
+++ client/src/router/AppRouters.tsx
@@ -1,71 +1,53 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import { TemplateLayout } from "../layout/tamplate-layout/TamplateLayout";
 import { Products } from "../page/Products/Products";
 import Login from "../page/Login/Login";
 import Cart from "../page/Cart/Cart";
 import Summary from "../page/Summary/Summary";
 import CheckoutInformation from "../page/CheckoutInformation/CheckoutInformation";
-import { JSX } from "react";
 
-const AuthGuard = ({ children }: { children: JSX.Element }) => {
-  return children;
-  // const token = sessionStorage.getItem("token");
+const AuthGuard = () => {
+  const location = useLocation();
 
-  // return !token ? <Login /> : children;
+  const token = sessionStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
 };
 
 const AppRouters = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/products" />,
-  },
   {
     path: "/login",
     element: <Login />,
   },
   {
-    path: "/products",
-    element: (
-      <AuthGuard>
-        <TemplateLayout>
-          <Products />
-        </TemplateLayout>
-      </AuthGuard>
-    ),
-  },
-  {
-    path: "/cart",
-    element: (
-      <AuthGuard>
-        <TemplateLayout>
-          <Cart />
-        </TemplateLayout>
-      </AuthGuard>
-    ),
-  },
-  {
-    path: "/checkout",
-    element: <TemplateLayout children={<Cart />} />,
-  },
-  {
-    path: "/summary",
-    element: (
-      <AuthGuard>
-        <TemplateLayout>
-          <Summary />,
-        </TemplateLayout>
-      </AuthGuard>
-    ),
-  },
-  {
-    path: "/checkout-information",
-    element: (
-      <AuthGuard>
-        <TemplateLayout>
-          <CheckoutInformation />
-        </TemplateLayout>
-      </AuthGuard>
-    ),
+    path: "/",
+    element: <AuthGuard />,
+    children: [
+      {
+        path: "",
+        element: (
+          <TemplateLayout>
+            <Outlet />
+          </TemplateLayout>
+        ),
+        children: [
+          { path: "", element: <Navigate to="/products" /> },
+          { path: "products", element: <Products /> },
+          { path: "cart", element: <Cart /> },
+          { path: "summary", element: <Summary /> },
+          { path: "checkout-information", element: <CheckoutInformation /> },
+        ],
+      },
+    ],
   },
 ]);
 
